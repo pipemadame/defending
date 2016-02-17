a354767465ade7e58ed696ea035c4cba873fdc1f
jeremybini <jeremybini@gmail.com>
2016-02-17 17:52:24 -0500

    init commit

---
+++ server/auth/github.oauth.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var router = require('express').Router();
+var passport = require('passport');
+var GitHubStrategy = require('passport-github').Strategy;
+var githubSecret = require('../../config/keys.js').github;
+var User = require('../api/users/user.model');
+
+router.get('/', passport.authenticate('github'));
+
+router.get('/callback', passport.authenticate('github', {
+	successRedirect: '/stories',
+	failureRedirect: '/signup'
+}));
+
+passport.use(new GitHubStrategy(githubSecret, function (token, refreshToken, profile, done) { 
+	User.findOne({'github.id': profile.id }, function (err, user) {
+		if (err) done(err);
+		else if (user) done(null, user);
+		else {
+			var email = profile.emails ? profile.emails[0].value : (profile.displayName.replace(/\s+/g, '_') + '@fake-auther-email.com');
+			//var email = profile.emails[0].value;
+			User.create({
+				email: email,
+				name: profile.displayName,
+				github: {
+					id: profile.id,
+					name: profile.displayName,
+					email: email,
+					token: token
+				}
+			}, done);
+		}
+	});
+}));
+
+module.exports = router;

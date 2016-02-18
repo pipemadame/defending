d2a133d3ec9e0da607b290a86ead35342af67d46
Liana <lrc47@cornell.edu>
2016-02-18 11:38:07 -0500

    ready for battle

--- server/api/stories/story.router.js
+++ server/api/stories/story.router.js
@@ -25,15 +25,18 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/', function (req, res, next) {
-	req.body.isAdmin = false;
-	Story.create(req.body)
-	.then(function (story) {
-		return story.populateAsync('author');
-	})
-	.then(function (populated) {
-		res.status(201).json(populated);
-	})
-	.then(null, next);
+	if (req.user.isAdmin || (req.user._id === req.body.author._id)) {
+		Story.create(req.body)
+		.then(function (story) {
+			return story.populateAsync('author');
+		})
+		.then(function (populated) {
+			res.status(201).json(populated);
+		})
+		.then(null, next);
+	} else {
+		res.sendStatus(401);
+	}
 });
 
 router.get('/:id', function (req, res, next) {
@@ -45,20 +48,28 @@ router.get('/:id', function (req, res, next) {
 });
 
 router.put('/:id', function (req, res, next) {
-	_.extend(req.story, req.body);
-	req.story.save()
-	.then(function (story) {
-		res.json(story);
-	})
-	.then(null, next);
+	if (req.user.isAdmin || (req.user._id === req.body.author._id)) {
+		_.extend(req.story, req.body);
+		req.story.save()
+		.then(function (story) {
+			res.json(story);
+		})
+		.then(null, next);
+	} else {
+		res.sendStatus(401);
+	}
 });
 
 router.delete('/:id', function (req, res, next) {
-	req.story.remove()
-	.then(function () {
-		res.status(204).end();
-	})
-	.then(null, next);
+	if (req.user.isAdmin || (req.user._id === req.body.author._id)) {
+		req.story.remove()
+		.then(function () {
+			res.status(204).end();
+		})
+		.then(null, next);
+	} else {
+		res.sendStatus(401);
+	}
 });
 
 module.exports = router;
--- server/api/users/user.router.js
+++ server/api/users/user.router.js
@@ -19,12 +19,22 @@ router.param('id', function (req, res, next, id) {
 router.get('/', function (req, res, next) {
 	User.find({}).exec()
 	.then(function (users) {
-		res.json(users);
+		res.json(users.map(function (user) {
+			return {
+				_id: user._id,
+				name: user.name,
+				photo: user.photo,
+				phone: user.phone,
+				email: user.email,
+				isAdmin: user.isAdmin
+			};
+		}));
 	})
 	.then(null, next);
 });
 
 router.post('/', function (req, res, next) {
+	req.body.isAdmin = false;
 	User.create(req.body)
 	.then(function (user) {
 		res.status(201).json(user);
@@ -37,26 +47,45 @@ router.get('/:id', function (req, res, next) {
 	.then(function (stories) {
 		var obj = req.requestedUser.toObject();
 		obj.stories = stories;
-		res.json(obj);
+		res.json({
+			_id: obj._id,
+			name: obj.name,
+			photo: obj.photo,
+			phone: obj.phone,
+			email: obj.email,
+			isAdmin: obj.isAdmin,
+			stories: obj.stories
+		});
 	})
 	.then(null, next);
 });
 
 router.put('/:id', function (req, res, next) {
-	_.extend(req.requestedUser, req.body);
-	req.requestedUser.save()
-	.then(function (user) {
-		res.json(user);
-	})
-	.then(null, next);
+	if (!req.user.isAdmin) {
+		req.body.isAdmin === false;
+	}
+	if (req.user.isAdmin || (req.user._id === req.requestedUser._id)) {
+		_.extend(req.requestedUser, req.body);
+		req.requestedUser.save()
+		.then(function (user) {
+			res.json(user);
+		})
+		.then(null, next);
+	} else {
+		res.sendStatus(401);
+	}
 });
 
 router.delete('/:id', function (req, res, next) {
-	req.requestedUser.remove()
-	.then(function () {
-		res.status(204).end();
-	})
-	.then(null, next);
+	if (req.user.isAdmin || (req.user._id === req.requestedUser._id)) {
+		req.requestedUser.remove()
+		.then(function () {
+			res.status(204).end();
+		})
+		.then(null, next);
+	} else {
+		res.sendStatus(401);
+	}
 });
 
 module.exports = router;

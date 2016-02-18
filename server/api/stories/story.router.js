7d4b74dc1beced6ea8898a743e483fe418c063e5
Liana <lrc47@cornell.edu>
2016-02-18 11:48:59 -0500

    more ready

--- server/api/stories/story.router.js
+++ server/api/stories/story.router.js
@@ -25,15 +25,19 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/', function (req, res, next) {
-	if (req.user.isAdmin || (req.user._id === req.body.author._id)) {
-		Story.create(req.body)
-		.then(function (story) {
-			return story.populateAsync('author');
-		})
-		.then(function (populated) {
-			res.status(201).json(populated);
-		})
-		.then(null, next);
+	if (req.user) {
+		if (req.user.isAdmin || (req.user._id === req.body.author._id)) {
+			Story.create(req.body)
+			.then(function (story) {
+				return story.populateAsync('author');
+			})
+			.then(function (populated) {
+				res.status(201).json(populated);
+			})
+			.then(null, next);
+		} else {
+			res.sendStatus(401);
+		}
 	} else {
 		res.sendStatus(401);
 	}
@@ -48,25 +52,33 @@ router.get('/:id', function (req, res, next) {
 });
 
 router.put('/:id', function (req, res, next) {
-	if (req.user.isAdmin || (req.user._id === req.body.author._id)) {
-		_.extend(req.story, req.body);
-		req.story.save()
-		.then(function (story) {
-			res.json(story);
-		})
-		.then(null, next);
+	if (req.user) {
+		if (req.user.isAdmin || (req.user._id === req.body.author._id)) {
+			_.extend(req.story, req.body);
+			req.story.save()
+			.then(function (story) {
+				res.json(story);
+			})
+			.then(null, next);
+		} else {
+			res.sendStatus(401);
+		}
 	} else {
 		res.sendStatus(401);
 	}
 });
 
 router.delete('/:id', function (req, res, next) {
-	if (req.user.isAdmin || (req.user._id === req.body.author._id)) {
-		req.story.remove()
-		.then(function () {
-			res.status(204).end();
-		})
-		.then(null, next);
+	if (req.user) {
+		if (req.user.isAdmin || (req.user._id === req.body.author._id)) {
+			req.story.remove()
+			.then(function () {
+				res.status(204).end();
+			})
+			.then(null, next);
+		} else {
+			res.sendStatus(401);
+		}
 	} else {
 		res.sendStatus(401);
 	}
